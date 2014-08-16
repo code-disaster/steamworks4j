f04d9959834578640026c636efe50b7927d38b2a
Daniel Ludwig <codi@code-disaster.com>
2014-08-16 14:20:20 +0200

    Simple test application for Steam API initialisation & user stats request/store.

---
+++ src/test/java/com/codedisaster/steamworks/SteamAPITestApplication.java
@@ -0,0 +1,114 @@
+package com.codedisaster.steamworks;
+
+import java.util.Scanner;
+
+public class SteamAPITestApplication {
+
+	private SteamUserStats userStats;
+
+	private SteamUserStatsCallback userStatsCallback = new SteamUserStatsCallback() {
+		@Override
+		public void onUserStatsReceived(long gameId, long userId, int result) {
+			System.out.println("User stats received: gameId=" + gameId + ", userId=" + userId +
+					", result=" + SteamResult.byValue(result).toString());
+		}
+
+		@Override
+		public void onUserStatsStored(long gameId, int result) {
+			System.out.println("User stats stored: gameId=" + gameId +
+					", result=" + SteamResult.byValue(result).toString());
+		}
+	};
+
+	class InputHandler implements Runnable {
+
+		private volatile boolean alive;
+		private Thread mainThread;
+		private Scanner scanner;
+
+		public InputHandler(Thread mainThread) {
+			this.alive = true;
+			this.mainThread = mainThread;
+			this.scanner = new Scanner(System.in);
+		}
+
+		@Override
+		public void run() {
+			while (alive && mainThread.isAlive()) {
+
+				if (scanner.hasNext()) {
+					String input = scanner.next();
+
+					if (input.startsWith("q")) {
+						alive = false;
+					}
+
+					if (input.equals("1")) {
+						userStats.requestCurrentStats();
+					} else if (input.equals("2")) {
+						userStats.storeStats();
+					}
+				}
+
+			}
+		}
+
+		public boolean alive() {
+			return alive;
+		}
+	}
+
+	private boolean run(@SuppressWarnings("unused") String[] arguments) throws SteamException {
+
+		System.out.println("Initialise Steam API ...");
+		if (!SteamAPI.init()) {
+			return false;
+		}
+
+		System.out.println("Register user stats callback ...");
+		userStats = new SteamUserStats(SteamAPI.getSteamUserStatsPointer(), userStatsCallback);
+
+		System.out.println("Requesting user stats ...");
+		userStats.requestCurrentStats();
+
+		InputHandler inputHandler = new InputHandler(Thread.currentThread());
+		new Thread(inputHandler).start();
+
+		while (inputHandler.alive() && SteamAPI.isSteamRunning()) {
+
+			// process callbacks
+			SteamAPI.runCallbacks();
+
+			try {
+				// sleep a little (Steam says it should poll at least 15 times/second)
+				Thread.sleep(1000 / 15);
+			} catch (InterruptedException e) {
+				// ignore
+			}
+		}
+
+		System.out.println("Shutting down Steam API ...");
+		SteamAPI.shutdown();
+
+		System.out.println("Bye!");
+		return true;
+	}
+
+	public static void main(String[] arguments) {
+
+		try {
+
+			System.load(arguments[0] + "/libsteam_api.dylib");
+			System.load(arguments[0] + "/libsteamworks4j.dylib");
+
+			if (!new SteamAPITestApplication().run(arguments)) {
+				System.exit(-1);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
+}
