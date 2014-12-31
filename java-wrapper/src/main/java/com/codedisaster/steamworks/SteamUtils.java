7437eee7f7f9e2c368839461dabf7cb8de22892e
Daniel Ludwig <codi@code-disaster.com>
2014-12-31 16:47:28 +0100

    Added SetOverlayNotificationPosition().

--- java-wrapper/src/main/java/com/codedisaster/steamworks/SteamUtils.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/SteamUtils.java
@@ -2,6 +2,13 @@ package com.codedisaster.steamworks;
 
 public class SteamUtils extends SteamInterface {
 
+	enum NotificationPosition {
+		TopLeft,
+		TopRight,
+		BottomLeft,
+		BottomRight
+	}
+
 	public SteamUtils(long pointer) {
 		super(pointer);
 	}
@@ -11,6 +18,10 @@ public class SteamUtils extends SteamInterface {
 
 	public long getAppID() {
 		return getAppID(pointer);
+	}
+
+	public void setOverlayNotificationPosition(NotificationPosition position) {
+		setOverlayNotificationPosition(pointer, position.ordinal());
 	}
 
 	public boolean isOverlayEnabled() {
@@ -26,6 +37,11 @@ public class SteamUtils extends SteamInterface {
 	static private native long getAppID(long pointer); /*
 		ISteamUtils* utils = (ISteamUtils*) pointer;
 		return (int64) utils->GetAppID();
+	*/
+
+	static private native void setOverlayNotificationPosition(long pointer, int position); /*
+		ISteamUtils* utils = (ISteamUtils*) pointer;
+		utils->SetOverlayNotificationPosition((ENotificationPosition) position);
 	*/
 
 	static private native boolean isOverlayEnabled(long pointer); /*
