e866fb562214d403adef514413cbea7e79f33a81
Daniel Ludwig <codi@code-disaster.com>
2015-03-07 01:19:12 +0100

    Windows: changed native build script to use premake5 / VS2013.

--- build-natives/build-win.bat
+++ build-natives/build-win.bat
@@ -1,7 +1,7 @@
-premake4 --file=build-win.lua vs2010
-msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=Win32 /t:Clean,Build
+premake5 --file=build-win.lua vs2013
+msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=Win32 /t:Rebuild
 move steamworks4j.dll ..\natives\libs\steamworks4j.dll
 copy ..\sdk\redistributable_bin\steam_api.dll ..\natives\libs\steam_api.dll
-msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=x64 /t:Clean,Build
+msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=x64 /t:Rebuild
 move steamworks4j.dll ..\natives\libs\steamworks4j64.dll
 copy ..\sdk\redistributable_bin\win64\steam_api64.dll ..\natives\libs\steam_api64.dll
--- build-natives/build-win.lua
+++ build-natives/build-win.lua
@@ -1,5 +1,6 @@
 solution "steamworks4j"
 	configurations { "ReleaseDLL" }
+	platforms { "x32", "x64" }
 
 	project "steamworks4j"
 
@@ -16,13 +17,13 @@ solution "steamworks4j"
 
 		defines { "NDEBUG", "WINDOWS" }
 		flags { "Optimize", "StaticRuntime" }
+		buildoptions { "/wd4244", "/wd4800", "/wd4996" }
 
-		configuration "x32"
+		filter "platforms:x32"
 			libdirs { "../sdk/redistributable_bin" }
 			links { "steam_api" }
 
-		configuration "x64"
+		filter "platforms:x64"
 			libdirs { "../sdk/redistributable_bin/win64" }
 			links { "steam_api64" }
 
-		platforms { "x32", "x64" }
