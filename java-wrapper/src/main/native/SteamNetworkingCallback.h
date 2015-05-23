9bf2cecef60924eda9bb8b27fd3429ebae495d5a
Daniel Ludwig <codi@code-disaster.com>
2015-05-23 17:30:14 +0200

    Fixed networking client callbacks.

--- java-wrapper/src/main/native/SteamNetworkingCallback.h
+++ java-wrapper/src/main/native/SteamNetworkingCallback.h
@@ -9,6 +9,9 @@ public:
 	SteamNetworkingCallback(JNIEnv* env, jobject callback);
 	~SteamNetworkingCallback();
 
-	STEAM_GAMESERVER_CALLBACK(SteamNetworkingCallback, onP2PSessionConnectFail, P2PSessionConnectFail_t, m_CallbackP2PSessionConnectFail);
-	STEAM_GAMESERVER_CALLBACK(SteamNetworkingCallback, onP2PSessionRequest, P2PSessionRequest_t, m_CallbackP2PSessionRequest);
+	STEAM_CALLBACK(SteamNetworkingCallback, onP2PSessionConnectFail, P2PSessionConnectFail_t, m_CallbackP2PSessionConnectFail);
+	STEAM_CALLBACK(SteamNetworkingCallback, onP2PSessionRequest, P2PSessionRequest_t, m_CallbackP2PSessionRequest);
+
+	STEAM_GAMESERVER_CALLBACK(SteamNetworkingCallback, onGameServerP2PSessionConnectFail, P2PSessionConnectFail_t, m_CallbackGameServerP2PSessionConnectFail);
+	STEAM_GAMESERVER_CALLBACK(SteamNetworkingCallback, onGameServerP2PSessionRequest, P2PSessionRequest_t, m_CallbackGameServerP2PSessionRequest);
 };
