514b198956a637f1cc8473582ca90009f410e4bb
Daniel Ludwig <codi@code-disaster.com>
2015-05-31 22:57:41 +0200

    Networking interface: cleaner API, more error checks, less heap allocations.

--- java-wrapper/src/main/java/com/codedisaster/steamworks/SteamNetworking.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/SteamNetworking.java
@@ -1,6 +1,6 @@
 package com.codedisaster.steamworks;
 
-import java.nio.Buffer;
+import java.nio.ByteBuffer;
 
 /**
  *
@@ -34,39 +34,97 @@ public class SteamNetworking extends SteamInterface {
 		}
 	}
 
+	private final int[] tmpIntResult = new int[1];
+	private final long[] tmpLongResult = new long[1];
+
 	public SteamNetworking(SteamNetworkingCallback callback, API api) {
 		super(api == API.Client ? SteamAPI.getSteamNetworkingPointer()
 				: SteamGameServerAPI.getSteamGameServerNetworkingPointer(),
 				createCallback(new SteamNetworkingCallbackAdapter(callback), api == API.Client));
 	}
 
-	public boolean sendP2PPacket(SteamID steamIDRemote, Buffer data, P2PSend sendType, int channel) throws SteamException {
+	/**
+	 * Sends packet data from a direct {@link ByteBuffer}.
+	 *
+	 * The packet data sent ranges from <code>ByteBuffer.position()</code> to <code>ByteBuffer.limit()</code>.
+	 */
+	public boolean sendP2PPacket(SteamID steamIDRemote, ByteBuffer data,
+								 P2PSend sendType, int channel) throws SteamException {
+
+		int offset = data.position();
+		int size = data.limit() - offset;
+		return sendP2PPacket(steamIDRemote, data, offset, size, sendType, channel);
+	}
+
+	/**
+	 * Sends packet data from a direct {@link ByteBuffer}.
+	 *
+	 * This function ignores the buffer's internal position and limit properties.
+	 */
+	public boolean sendP2PPacket(SteamID steamIDRemote, ByteBuffer data, int offset, int size,
+								 P2PSend sendType, int channel) throws SteamException {
+
 		if (!data.isDirect()) {
 			throw new SteamException("Direct buffer required!");
 		}
-		return sendP2PPacket(pointer, steamIDRemote.handle, data, data.limit(), sendType.ordinal(), channel);
+
+		if (data.capacity() < offset + size) {
+			throw new SteamException("Buffer capacity exceeded!");
+		}
+
+		return sendP2PPacket(pointer, steamIDRemote.handle, data, offset, size, sendType.ordinal(), channel);
 	}
 
 	public int isP2PPacketAvailable(int channel) {
-		int[] msgSize = new int[1];
-		if (isP2PPacketAvailable(pointer, msgSize, channel)) {
-			return msgSize[0];
+		if (isP2PPacketAvailable(pointer, tmpIntResult, channel)) {
+			return tmpIntResult[0];
 		}
 		return 0;
 	}
 
-	public SteamID readP2PPacket(Buffer dest, int channel) throws SteamException {
+	/**
+	 * Read incoming packet data into a direct {@link ByteBuffer}.
+	 *
+	 * The packet data is stored starting at <code>ByteBuffer.position()</code>, up to <code>ByteBuffer.limit()</code>.
+	 * On return, the buffer limit is set to <code>ByteBuffer.position()</code> plus the number of bytes received.
+	 *
+	 * On success, returns the number of bytes received, and the <code>steamIDRemote</code> parameter contains the
+	 * sender's ID.
+	 */
+	public int readP2PPacket(SteamID steamIDRemote, ByteBuffer dest, int channel) throws SteamException {
+		int offset = dest.position();
+		int capacity = dest.limit() - offset;
+		return readP2PPacket(steamIDRemote, dest, offset, capacity, channel);
+	}
+
+	/**
+	 * Read incoming packet data into a direct {@link ByteBuffer}.
+	 *
+	 * This function ignores the buffer's internal position and limit properties. On return, the buffer position is set
+	 * to <code>offset</code>, the buffer limit is set to <code>offset</code> plus the number of bytes received.
+	 *
+	 * On success, returns the number of bytes received, and the <code>steamIDRemote</code> parameter contains the
+	 * sender's ID.
+	 */
+	public int readP2PPacket(SteamID steamIDRemote, ByteBuffer dest,
+							 int offset, int capacity, int channel) throws SteamException {
+
 		if (!dest.isDirect()) {
 			throw new SteamException("Direct buffer required!");
 		}
-		int[] msgSizeInBytes = new int[1];
-		long[] steamIDRemote = new long[1];
-		if (readP2PPacket(pointer, dest, dest.capacity(), msgSizeInBytes, steamIDRemote, channel)) {
-			dest.position(0);
-			dest.limit(msgSizeInBytes[0]);
-			return new SteamID(steamIDRemote[0]);
+
+		if (dest.capacity() < offset + capacity) {
+			throw new SteamException("Buffer capacity exceeded!");
 		}
-		return null;
+
+		if (readP2PPacket(pointer, dest, offset, capacity, tmpIntResult, tmpLongResult, channel)) {
+			dest.position(offset);
+			dest.limit(offset + tmpIntResult[0]);
+			steamIDRemote.handle = tmpLongResult[0];
+			return tmpIntResult[0];
+		}
+
+		return 0;
 	}
 
 	public boolean acceptP2PSessionWithUser(SteamID steamIDRemote) {
@@ -100,9 +158,11 @@ public class SteamNetworking extends SteamInterface {
 		}
 	*/
 
-	static private native boolean sendP2PPacket(long pointer, long steamIDRemote, Buffer data, int sizeInBytes, int sendType, int channel); /*
+	static private native boolean sendP2PPacket(long pointer, long steamIDRemote, ByteBuffer data,
+												int offset, int size, int sendType, int channel); /*
+
 		ISteamNetworking* net = (ISteamNetworking*) pointer;
-		return net->SendP2PPacket((uint64) steamIDRemote, data, sizeInBytes, (EP2PSend) sendType, channel);
+		return net->SendP2PPacket((uint64) steamIDRemote, &data[offset], size, (EP2PSend) sendType, channel);
 	*/
 
 	static private native boolean isP2PPacketAvailable(long pointer, int[] msgSize, int channel); /*
@@ -110,10 +170,12 @@ public class SteamNetworking extends SteamInterface {
 		return net->IsP2PPacketAvailable((uint32 *)msgSize, channel);
 	*/
 
-	static private native boolean readP2PPacket(long pointer, Buffer dest, int capacity, int[] msgSizeInBytes, long[] steamIDRemote, int channel); /*
+	static private native boolean readP2PPacket(long pointer, ByteBuffer dest, int offset, int capacity,
+												int[] msgSizeInBytes, long[] steamIDRemote, int channel); /*
+
 		ISteamNetworking* net = (ISteamNetworking*) pointer;
 		CSteamID remote;
-		if (net->ReadP2PPacket(dest, capacity, (uint32*) msgSizeInBytes, &remote, channel)) {
+		if (net->ReadP2PPacket(&dest[offset], capacity, (uint32*) msgSizeInBytes, &remote, channel)) {
 			steamIDRemote[0] = remote.ConvertToUint64();
 			return true;
 		}
