4e98ea33f3ee5d7e6642e2bc1231265861ae38a2
Daniel Ludwig <codi@code-disaster.com>
2015-06-09 22:42:09 +0200

    Moved native code generator to separate sub-project. Removed dependency of shared library loader on gdx-jnigen.

--- java-wrapper/src/main/java/com/codedisaster/steamworks/SteamSharedLibraryLoader.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/SteamSharedLibraryLoader.java
@@ -1,58 +1,45 @@
 package com.codedisaster.steamworks;
 
-import com.badlogic.gdx.jnigen.JniGenSharedLibraryLoader;
-import com.badlogic.gdx.jnigen.SharedLibraryFinder;
-
 import java.io.*;
+import java.util.zip.CRC32;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-class SteamSharedLibraryLoader extends JniGenSharedLibraryLoader {
+class SteamSharedLibraryLoader {
 
-	private String firstCrc;
+	private String libraryPath;
+	private boolean fromJar;
 
-	private SharedLibraryFinder libraryFinder = new SharedLibraryFinder() {
-		@Override
-		public String getSharedLibraryNameWindows(String sharedLibName, boolean is64Bit, ZipFile nativesJar) {
-			return sharedLibName + (is64Bit ? "64" : "") + ".dll";
-		}
-
-		@Override
-		public String getSharedLibraryNameLinux(String sharedLibName, boolean is64Bit, ZipFile nativesJar) {
-			return "lib" + sharedLibName + (is64Bit ? "64" : "") + ".so";
-		}
-
-		@Override
-		public String getSharedLibraryNameMac(String sharedLibName, boolean is64Bit, ZipFile nativesJar) {
-			return "lib" + sharedLibName + ".dylib";
-		}
-
-		@Override
-		public String getSharedLibraryNameAndroid(String sharedLibName, ZipFile nativesJar) {
-			return null;
-		}
-	};
+	private String libraryCrc;
 
 	private static boolean alreadyLoaded = false;
 
-	private SteamSharedLibraryLoader() {
-		super();
-	}
+	private SteamSharedLibraryLoader(String libraryPath, boolean fromJar) {
+		this.libraryPath = libraryPath;
+		this.fromJar = fromJar;
 
-	private SteamSharedLibraryLoader(String nativesJar) {
-		super(nativesJar);
-
-		setSharedLibraryFinder(libraryFinder);
-
-		if (nativesJar != null) {
+		if (libraryPath != null && fromJar) {
 			try {
-				firstCrc = crc(new FileInputStream(new File(nativesJar)));
+				libraryCrc = crc(new FileInputStream(new File(libraryPath)));
 			} catch (FileNotFoundException e) {
-				firstCrc = Integer.toHexString(nativesJar.hashCode());
+				libraryCrc = Integer.toHexString(libraryPath.hashCode());
 			}
 		}
 	}
 
-	private void loadLibraries(String libraryPath, boolean fromJar, String... libraryNames) throws IOException {
+	private String getLibNameWindows(String sharedLibName, boolean is64Bit) {
+		return sharedLibName + (is64Bit ? "64" : "") + ".dll";
+	}
+
+	private String getLibNameLinux(String sharedLibName, boolean is64Bit) {
+		return "lib" + sharedLibName + (is64Bit ? "64" : "") + ".so";
+	}
+
+	private String getLibNameMac(String sharedLibName) {
+		return "lib" + sharedLibName + ".dylib";
+	}
+
+	private void loadLibraries(String... libraryNames) throws IOException {
 		String osName = System.getProperty("os.name");
 		String osArch = System.getProperty("os.arch");
 
@@ -63,35 +50,84 @@ class SteamSharedLibraryLoader extends JniGenSharedLibraryLoader {
 		boolean is64Bit = osArch.equals("amd64") || osArch.equals("x86_64");
 
 		for (String libraryName : libraryNames) {
-			if (fromJar) {
-				load(libraryName);
-			} else {
-				String fullName = "";
 
-				if (isWindows) {
-					fullName = libraryFinder.getSharedLibraryNameWindows(libraryName, is64Bit, null);
-				}
-				if (isLinux) {
-					fullName = libraryFinder.getSharedLibraryNameLinux(libraryName, is64Bit, null);
-				}
-				if (isMac) {
-					fullName = libraryFinder.getSharedLibraryNameMac(libraryName, is64Bit, null);
-				}
+			String librarySystemName = "";
 
-				String fullPath = libraryPath + "/" + fullName;
-				String absolutePath = new File(fullPath).getCanonicalPath();
-				System.load(absolutePath);
+			if (isWindows) {
+				librarySystemName = getLibNameWindows(libraryName, is64Bit);
 			}
+			if (isLinux) {
+				librarySystemName = getLibNameLinux(libraryName, is64Bit);
+			}
+			if (isMac) {
+				librarySystemName = getLibNameMac(libraryName);
+			}
+
+			String fullPath;
+
+			if (fromJar) {
+				// extract library from Jar into temp folder
+				fullPath = extractLibrary(librarySystemName);
+			} else {
+				// load native library directly from specified path
+				fullPath = libraryPath + "/" + librarySystemName;
+			}
+
+			String absolutePath = new File(fullPath).getCanonicalPath();
+			System.load(absolutePath);
 		}
 	}
 
-	@Override
-	public String crc(InputStream input) {
-		if (firstCrc != null) {
-			return firstCrc;
+	private String extractLibrary(String sharedLibName) throws IOException {
+
+		File nativesPath = new File(System.getProperty("java.io.tmpdir") + "/steamworks4j/" + libraryCrc);
+		File nativeFile = new File(nativesPath, sharedLibName);
+
+		if (!nativesPath.exists()) {
+			if (!nativesPath.mkdirs()) {
+				throw new IOException("Error creating temp folder: " + nativesPath.getCanonicalPath());
+			}
 		}
-		firstCrc = super.crc(input);
-		return firstCrc;
+
+		ZipFile zip = new ZipFile(libraryPath);
+		ZipEntry entry = zip.getEntry(sharedLibName);
+		InputStream input = zip.getInputStream(entry);
+
+		if (input == null) {
+			throw new IOException("Error extracting " + sharedLibName + " from " + libraryPath);
+		}
+
+		FileOutputStream output = new FileOutputStream(nativeFile);
+		byte[] buffer = new byte[4096];
+		while (true) {
+			int length = input.read(buffer);
+			if (length == -1) break;
+			output.write(buffer, 0, length);
+		}
+		input.close();
+		output.close();
+
+		return nativeFile.getAbsolutePath();
+	}
+
+	private String crc(InputStream input) {
+		CRC32 crc = new CRC32();
+		byte[] buffer = new byte[4096];
+		try {
+			while (true) {
+				int length = input.read(buffer);
+				if (length == -1) break;
+				crc.update(buffer, 0, length);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				input.close();
+			} catch (IOException ignored) {
+			}
+		}
+		return Long.toHexString(crc.getValue());
 	}
 
 	static boolean extractAndLoadLibraries(boolean fromJar, String libraryPath) {
@@ -106,6 +142,9 @@ class SteamSharedLibraryLoader extends JniGenSharedLibraryLoader {
 
 			if (libraryPath == null) {
 
+				// if no library path is specified, extract steamworks4j-natives.jar from
+				// resource path into temporary folder
+
 				libraryPath = System.getProperty("java.io.tmpdir") + "/steamworks4j/steamworks4j-natives.jar";
 
 				File libraryDirectory = new File(libraryPath).getParentFile();
@@ -117,7 +156,7 @@ class SteamSharedLibraryLoader extends JniGenSharedLibraryLoader {
 
 				try {
 
-					InputStream input = SteamAPI.class.getResourceAsStream("/steamworks4j-natives.jar");
+					InputStream input = SteamSharedLibraryLoader.class.getResourceAsStream("/steamworks4j-natives.jar");
 					FileOutputStream output = new FileOutputStream(new File(libraryPath));
 
 					byte[] cache = new byte[4096];
@@ -140,16 +179,12 @@ class SteamSharedLibraryLoader extends JniGenSharedLibraryLoader {
 
 			}
 
-			loader = new SteamSharedLibraryLoader(libraryPath);
-
-		} else {
-
-			loader = new SteamSharedLibraryLoader();
-
 		}
 
+		loader = new SteamSharedLibraryLoader(libraryPath, fromJar);
+
 		try {
-			loader.loadLibraries(libraryPath, fromJar, "steam_api", "steamworks4j");
+			loader.loadLibraries("steam_api", "steamworks4j");
 		} catch (IOException e) {
 			e.printStackTrace();
 			return false;
--- jnigen/src/main/java/com/codedisaster/steamworks/jnigen/JNICodeGenerator.java
+++ jnigen/src/main/java/com/codedisaster/steamworks/jnigen/JNICodeGenerator.java
@@ -2,14 +2,19 @@ package com.codedisaster.steamworks.jnigen;
 
 import com.badlogic.gdx.jnigen.NativeCodeGenerator;
 
+import java.io.File;
+
 public class JNICodeGenerator {
 
 	public static void main(String[] arguments) {
 
 		try {
-
-			new NativeCodeGenerator().generate("src/main/java", "target/classes", "src/main/native",
-					new String[] { "**/*.java" }, new String[] { "**/JNICodeGenerator.java", "**/SteamSharedLibraryLoader.java" });
+			new NativeCodeGenerator().generate(
+					"src/main/java",
+					"target/classes",
+					"src/main/native",
+					new String[] { "**/*.java" },
+					new String[] { "**/JNICodeGenerator.java", "**/SteamSharedLibraryLoader.java" });
 
 		} catch (Exception e) {
 			e.printStackTrace();
