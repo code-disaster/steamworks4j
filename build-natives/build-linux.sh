022cb793dfc5eb0c915b2bee70b39097885f08ca
Daniel Ludwig <codi@code-disaster.com>
2016-07-06 10:13:00 +0200

    Removed steamworks4j-natives subproject. Changed build scripts to copy to ./java-wrapper/src/main/resources.

--- build-natives/build-linux.sh
+++ build-natives/build-linux.sh
@@ -1,9 +1,9 @@
 premake4 --file=build-linux.lua gmake
 make clean config=release32
 make config=release32
-mv libsteamworks4j.so ../natives/libs/libsteamworks4j.so
-cp ../sdk/redistributable_bin/linux32/libsteam_api.so ../natives/libs/libsteam_api.so
+mv libsteamworks4j.so ../java-wrapper/src/main/resources/libsteamworks4j.so
+cp ../sdk/redistributable_bin/linux32/libsteam_api.so ../java-wrapper/src/main/resources/libsteam_api.so
 make clean config=release64
 make config=release64
-mv libsteamworks4j.so ../natives/libs/libsteamworks4j64.so
-cp ../sdk/redistributable_bin/linux64/libsteam_api.so ../natives/libs/libsteam_api64.so
+mv libsteamworks4j.so ../java-wrapper/src/main/resources/libsteamworks4j64.so
+cp ../sdk/redistributable_bin/linux64/libsteam_api.so ../java-wrapper/src/main/resources/libsteam_api64.so
--- build-natives/build-win.bat
+++ build-natives/build-win.bat
@@ -1,7 +1,7 @@
 premake5 --file=build-win.lua vs2013
 msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=Win32 /t:Rebuild
-move steamworks4j.dll ..\natives\libs\steamworks4j.dll
-copy ..\sdk\redistributable_bin\steam_api.dll ..\natives\libs\steam_api.dll
+move steamworks4j.dll ..\java-wrapper\src\main\resources\steamworks4j.dll
+copy ..\sdk\redistributable_bin\steam_api.dll ..\java-wrapper\src\main\resources\steam_api.dll
 msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=x64 /t:Rebuild
-move steamworks4j.dll ..\natives\libs\steamworks4j64.dll
-copy ..\sdk\redistributable_bin\win64\steam_api64.dll ..\natives\libs\steam_api64.dll
+move steamworks4j.dll ..\java-wrapper\src\main\resources\steamworks4j64.dll
+copy ..\sdk\redistributable_bin\win64\steam_api64.dll ..\java-wrapper\src\main\resources\steam_api64.dll
