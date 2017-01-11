96a2daf0caa854344eca2840e6f69f23d7e1e5c4
Daniel Ludwig <codi@code-disaster.com>
2017-01-11 14:05:54 +0100

    Changed SteamAPI.isSteamRunning() to not call the native function by default.

--- java-wrapper/src/main/java/com/codedisaster/steamworks/SteamAPI.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/SteamAPI.java
@@ -6,11 +6,11 @@ public class SteamAPI {
 
 	static private boolean isRunning = false;
 
-	static public boolean init() throws SteamException {
+	public static boolean init() throws SteamException {
 		return init(null);
 	}
 
-	static public boolean init(String libraryPath) throws SteamException {
+	public static boolean init(String libraryPath) throws SteamException {
 
 		SteamSharedLibraryLoader.loadLibraries(libraryPath);
 
@@ -19,13 +19,25 @@ public class SteamAPI {
 		return isRunning;
 	}
 
-	static public void shutdown() {
+	public static void shutdown() {
 		isRunning = false;
 		nativeShutdown();
 	}
 
-	static public boolean isSteamRunning() {
-		return isRunning && isSteamRunningNative();
+	/**
+	 * According to the documentation, SteamAPI_IsSteamRunning() should "only be used in very
+	 * specific cases". Also, there seems to be an issue with leaking OS resources on Mac OS X.
+	 * For these reasons, the default behaviour of this function has been changed to <i>not</i>
+	 * call the native function.
+	 *
+	 * @see SteamAPI#isSteamRunning(boolean)
+	 */
+	public static boolean isSteamRunning() {
+		return isSteamRunning(false);
+	}
+
+	public static boolean isSteamRunning(boolean checkNative) {
+		return isRunning && (!checkNative || isSteamRunningNative());
 	}
 
 	public static void printDebugInfo(PrintStream stream) {
@@ -46,11 +58,11 @@ public class SteamAPI {
 		static JavaVM* staticVM = 0;
 	*/
 
-	static public native boolean restartAppIfNecessary(int appId); /*
+	public static native boolean restartAppIfNecessary(int appId); /*
 		return SteamAPI_RestartAppIfNecessary(appId);
 	*/
 
-	static private native boolean nativeInit(); /*
+	private static native boolean nativeInit(); /*
 		if (env->GetJavaVM(&staticVM) != 0) {
 			return false;
 		}
@@ -58,15 +70,15 @@ public class SteamAPI {
 		return SteamAPI_Init();
 	*/
 
-	static private native void nativeShutdown(); /*
+	private static native void nativeShutdown(); /*
 		SteamAPI_Shutdown();
 	*/
 
-	static public native void runCallbacks(); /*
+	public static native void runCallbacks(); /*
 		SteamAPI_RunCallbacks();
 	*/
 
-	static private native boolean isSteamRunningNative(); /*
+	private static native boolean isSteamRunningNative(); /*
 		return SteamAPI_IsSteamRunning();
 	*/
 
