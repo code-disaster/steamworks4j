d6b9c9c32f2dcc35f31a7625bab87d81333e9e8c
Daniel Ludwig <codi@code-disaster.com>
2017-03-26 23:49:28 +0200

    Added SteamNetworking.getP2PSessionState(). (#43)

--- java-wrapper/src/main/java/com/codedisaster/steamworks/SteamNetworking.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/SteamNetworking.java
@@ -34,6 +34,49 @@ public class SteamNetworking extends SteamInterface {
 		}
 	}
 
+	public static class P2PSessionState {
+		byte connectionActive;
+		byte connecting;
+		byte sessionError;
+		byte usingRelay;
+		int bytesQueuedForSend;
+		int packetsQueuedForSend;
+		int remoteIP;
+		short remotePort;
+
+		public boolean isConnectionActive() {
+			return connectionActive != 0;
+		}
+
+		public boolean isConnecting() {
+			return connecting != 0;
+		}
+
+		public P2PSessionError getLastSessionError() {
+			return P2PSessionError.byOrdinal(sessionError);
+		}
+
+		public boolean isUsingRelay() {
+			return usingRelay != 0;
+		}
+
+		public int getBytesQueuedForSend() {
+			return bytesQueuedForSend;
+		}
+
+		public int getPacketsQueuedForSend() {
+			return packetsQueuedForSend;
+		}
+
+		public int getRemoteIP() {
+			return remoteIP;
+		}
+
+		public short getRemotePort() {
+			return remotePort;
+		}
+	}
+
 	private final int[] tmpIntResult = new int[1];
 	private final long[] tmpLongResult = new long[1];
 
@@ -139,6 +182,10 @@ public class SteamNetworking extends SteamInterface {
 		return closeP2PChannelWithUser(pointer, steamIDRemote.handle, channel);
 	}
 
+	public boolean getP2PSessionState(SteamID steamIDRemote, P2PSessionState connectionState) {
+		return getP2PSessionState(pointer, steamIDRemote.handle, connectionState);
+	}
+
 	public boolean allowP2PPacketRelay(boolean allow) {
 		return allowP2PPacketRelay(pointer, allow);
 	}
@@ -197,7 +244,44 @@ public class SteamNetworking extends SteamInterface {
 		return net->CloseP2PChannelWithUser((uint64) steamIDRemote, channel);
 	*/
 
-	// [@code-disaster] note: removed GetP2PSessionState(), won't work that way
+	private static native boolean getP2PSessionState(long pointer, long steamIDRemote,
+													 P2PSessionState connectionState); /*
+
+		ISteamNetworking* net = (ISteamNetworking*) pointer;
+		P2PSessionState_t result;
+
+		if (net->GetP2PSessionState((uint64) steamIDRemote, &result)) {
+			jclass clazz = env->GetObjectClass(connectionState);
+
+			jfieldID field = env->GetFieldID(clazz, "connectionActive", "B");
+			env->SetByteField(connectionState, field, (jbyte) result.m_bConnectionActive);
+
+			field = env->GetFieldID(clazz, "connecting", "B");
+			env->SetByteField(connectionState, field, (jbyte) result.m_bConnecting);
+
+			field = env->GetFieldID(clazz, "sessionError", "B");
+			env->SetByteField(connectionState, field, (jbyte) result.m_eP2PSessionError);
+
+			field = env->GetFieldID(clazz, "usingRelay", "B");
+			env->SetByteField(connectionState, field, (jbyte) result.m_bUsingRelay);
+
+			field = env->GetFieldID(clazz, "bytesQueuedForSend", "I");
+			env->SetIntField(connectionState, field, result.m_nBytesQueuedForSend);
+
+			field = env->GetFieldID(clazz, "packetsQueuedForSend", "I");
+			env->SetIntField(connectionState, field, result.m_nPacketsQueuedForSend);
+
+			field = env->GetFieldID(clazz, "remoteIP", "I");
+			env->SetIntField(connectionState, field, (jint) result.m_nRemoteIP);
+
+			field = env->GetFieldID(clazz, "remotePort", "S");
+			env->SetShortField(connectionState, field, (jshort) result.m_nRemotePort);
+
+			return true;
+        }
+
+        return false;
+	*/
 
 	private static native boolean allowP2PPacketRelay(long pointer, boolean allow); /*
 		ISteamNetworking* net = (ISteamNetworking*) pointer;
