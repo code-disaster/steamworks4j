4a07f97044f54e4c49cdf165ec24dcc06d8feccc
Daniel Ludwig <codi@code-disaster.com>
2017-05-10 18:30:32 +0200

    Fixed OS X build script.

--- build-natives/build-osx.sh
+++ build-natives/build-osx.sh
@@ -1,5 +1,7 @@
 premake4 --file=build-osx.lua xcode3
-xcodebuild -alltargets clean
-xcodebuild -configuration release
+xcodebuild -project steamworks4j.xcodeproj -alltargets clean
+xcodebuild -project steamworks4j.xcodeproj -configuration ReleaseDLL
 cp build/ReleaseDLL/libsteamworks4j.dylib ../java-wrapper/src/main/resources/libsteamworks4j.dylib
+xcodebuild -project steamworks4j-server.xcodeproj -alltargets clean
+xcodebuild -project steamworks4j-server.xcodeproj -configuration ReleaseDLL
 cp build/ReleaseDLL/libsteamworks4j-server.dylib ../server/src/main/resources/libsteamworks4j-server.dylib
