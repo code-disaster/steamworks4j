c202bfc44b419241e1155f2f90c18ab6b37d5849
Daniel Ludwig <codi@code-disaster.com>
2017-05-17 17:09:57 +0200

    Do not auto-generate version info.

--- java-wrapper/src/main/java/com/codedisaster/steamworks/Version.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/Version.java
@@ -2,7 +2,7 @@ package com.codedisaster.steamworks;
 
 public final class Version {
 
-	private static final String VERSION = "${project.version}";
+	private static final String VERSION = "1.7.0-SNAPSHOT";
 
 	public static String getVersion() {
 		return VERSION;
