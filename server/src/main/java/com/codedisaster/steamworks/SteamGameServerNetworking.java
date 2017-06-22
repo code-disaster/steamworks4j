f3d76475a510e8b93d57186b388c58f460b36511
Alrik Geselle <lord_alrik@web.de>
2017-06-22 10:02:28 +0200

    Fixed SteamGameServerNetworking cionstructor not visible

--- server/src/main/java/com/codedisaster/steamworks/SteamGameServerNetworking.java
+++ server/src/main/java/com/codedisaster/steamworks/SteamGameServerNetworking.java
@@ -2,7 +2,7 @@ package com.codedisaster.steamworks;
 
 public class SteamGameServerNetworking extends SteamNetworking {
 
-	SteamGameServerNetworking(SteamNetworkingCallback callback) {
+	public SteamGameServerNetworking(SteamNetworkingCallback callback) {
 		super(SteamGameServerAPINative.getSteamGameServerNetworkingPointer(),
 				SteamGameServerNetworkingNative.createCallback(new SteamNetworkingCallbackAdapter(callback)));
 	}
