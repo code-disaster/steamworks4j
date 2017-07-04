24fc34238c3184d1846cf92d19304ad272751bab
Daniel Ludwig <codi@code-disaster.com>
2017-07-04 19:53:19 +0200

    Premake5 VS2017 support not quite ready yet. Pass action (vs2013, vs2015) as batch argument.

--- build-natives/build-win.bat
+++ build-natives/build-win.bat
@@ -1,4 +1,6 @@
-premake5 --file=build-win.lua vs2017
+@if [%1]==[] goto usage
+
+premake5 --file=build-win.lua %1
 msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=Win32 /t:Rebuild
 copy bin\x32\ReleaseDLL\steamworks4j.dll ..\java-wrapper\src\main\resources\steamworks4j.dll
 copy bin\x32\ReleaseDLL\steamworks4j-server.dll ..\server\src\main\resources\steamworks4j-server.dll
@@ -7,3 +9,9 @@ msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=x64 /t:Rebuild
 copy bin\x64\ReleaseDLL\steamworks4j.dll ..\java-wrapper\src\main\resources\steamworks4j64.dll
 copy bin\x64\ReleaseDLL\steamworks4j-server.dll ..\server\src\main\resources\steamworks4j-server64.dll
 copy bin\x64\ReleaseDLL\steamworks4j-encryptedappticket.dll ..\server\src\main\resources\steamworks4j-encryptedappticket64.dll
+
+@goto :eof
+
+:usage
+@echo call with [action] profile, e.g. "build-win vs2013"
+@exit /B 1
