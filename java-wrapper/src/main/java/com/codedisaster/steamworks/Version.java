a8ac20c5b2c0b5ac0a6f593c56e27425d13292f8
Daniel Ludwig <codi@code-disaster.com>
2018-10-02 14:50:20 +0200

    Updated snapshot version.

--- java-wrapper/src/main/java/com/codedisaster/steamworks/Version.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/Version.java
@@ -2,7 +2,7 @@ package com.codedisaster.steamworks;
 
 public final class Version {
 
-	private static final String VERSION = "1.8.0";
+	private static final String VERSION = "1.8.1-SNAPSHOT";
 
 	public static String getVersion() {
 		return VERSION;
