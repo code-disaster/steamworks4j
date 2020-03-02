f3625559a1e8f802329279bf609123b945133611
Daniel Ludwig <codi@code-disaster.com>
2020-03-02 16:04:51 +0100

    OS X: fix SDK paths.

--- java-wrapper/src/main/java/com/codedisaster/steamworks/SteamSharedLibraryLoader.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/SteamSharedLibraryLoader.java
@@ -88,7 +88,7 @@ class SteamSharedLibraryLoader {
 				path = new File(SDK_LIBRARY_PATH, "linux64");
 				break;
 			case MacOS:
-				path = new File(SDK_LIBRARY_PATH, "osx32");
+				path = new File(SDK_LIBRARY_PATH, "osx");
 				break;
 			default:
 				return null;
