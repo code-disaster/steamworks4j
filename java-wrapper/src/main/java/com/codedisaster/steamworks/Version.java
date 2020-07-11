95c71a1db0175c52bf98bfb40cd6a6853e25d19d
Fabian Karolat <fkarolat@yahoo.com>
2020-07-11 19:15:47 +0200

    Reverted Test-Version
    (cherry picked from commit c305d33e46d6d111c192ef273b3c21968af01dba)

--- java-wrapper/src/main/java/com/codedisaster/steamworks/Version.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/Version.java
@@ -2,7 +2,7 @@ package com.codedisaster.steamworks;
 
 public final class Version {
 
-	private static final String VERSION = "2.0.0-SNAPSHOT";
+	private static final String VERSION = "1.8.1-SNAPSHOT";
 
 	public static String getVersion() {
 		return VERSION;
