3326ce16c75af7384ca1e8258a26dc2474c455fb
Daniel Ludwig <codi@code-disaster.com>
2022-08-08 08:07:36 +0200

    Exit script on msbuild error

--- build-natives/build-win.bat
+++ build-natives/build-win.bat
@@ -2,10 +2,12 @@
 
 premake5 --file=build-win.lua %1
 msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=Win32 /t:Rebuild
+@if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 copy bin\x32\ReleaseDLL\steamworks4j.dll ..\java-wrapper\src\main\resources\steamworks4j.dll
 copy bin\x32\ReleaseDLL\steamworks4j-server.dll ..\server\src\main\resources\steamworks4j-server.dll
 copy bin\x32\ReleaseDLL\steamworks4j-encryptedappticket.dll ..\server\src\main\resources\steamworks4j-encryptedappticket.dll
 msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=x64 /t:Rebuild
+@if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 copy bin\x64\ReleaseDLL\steamworks4j.dll ..\java-wrapper\src\main\resources\steamworks4j64.dll
 copy bin\x64\ReleaseDLL\steamworks4j-server.dll ..\server\src\main\resources\steamworks4j-server64.dll
 copy bin\x64\ReleaseDLL\steamworks4j-encryptedappticket.dll ..\server\src\main\resources\steamworks4j-encryptedappticket64.dll
