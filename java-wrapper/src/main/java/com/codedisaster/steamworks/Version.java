f3939f7284ac3d774366a3991f978c9fd49e33c4
Tyler <tylerhasman10@hotmail.com>
2024-01-29 17:00:50 -0800

    Revert "Updated pom's"
    This reverts commit f00ec5bbc31e27906d2f829aa195610df4bee94a.

--- java-wrapper/src/main/java/com/codedisaster/steamworks/Version.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/Version.java
@@ -2,7 +2,7 @@ package com.codedisaster.steamworks;
 
 public final class Version {
 
-	private static final String VERSION = "1.10.0-NETWORK-SOCKETS";
+	private static final String VERSION = "1.10.0-SNAPSHOT";
 
 	public static String getVersion() {
 		return VERSION;
